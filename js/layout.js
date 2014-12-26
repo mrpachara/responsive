a00f522a29097d3b32ca1fb3af1341ea0aa5e511
mrPachara <mrpachara@gmail.com>
2014-12-26 18:31:16 +0700

    add media query

--- js/layout.js
+++ js/layout.js
@@ -10,4 +10,48 @@
 	}
 
 	$('head').append($link);
+
+	$(function(){
+		var $html = $('html');
+		var $lyTitle = $('#ly-title');
+
+		$(window).on('scroll.layout', function(ev){
+			var $this = $(this);
+			var scrollTop = $this.scrollTop();
+
+			$html[(scrollTop == 0)? 'removeClass' : 'addClass']('st-float');
+			/*
+			$('button.cmd-toggle').each(function(){
+				$('#' + $(this).val()).removeClass('st-active');
+			});
+			*/
+
+			if((scrollTop < $lyTitle.outerHeight()) && (($this.height() + scrollTop) < $(document).height())){
+				var window_height = $this.height();
+				var lyTitle_ratio = (window_height - scrollTop)/window_height;
+				if(lyTitle_ratio < 0) lyTitle_ratio = 0;
+
+				$lyTitle.attr('style', 'height: ' + (lyTitle_ratio * 100) + '%');
+
+				setTimeout(function(){
+					var lyTitle_opacityRatio = ($lyTitle.outerHeight() - $this.scrollTop()) / $this.height();
+					if(lyTitle_opacityRatio < 0) lyTitle_opacityRatio = 0;
+
+					$('.el-content', $lyTitle).attr('style', 'opacity: ' + lyTitle_opacityRatio + ';');
+				}, 1);
+			}
+		});
+
+		$(document).on('click.global', '.cmd-toggle', function(ev){
+			var $this = $(this);
+
+			var $target = $('#' + $this.val());
+			$target.removeAttr('style');
+
+			$target.toggleClass('st-active');
+			setTimeout(function(){
+				$target.attr('style', 'overflow-y: auto;');
+			}, 500);
+		});
+	});
 })(window.document, window.jQuery);
