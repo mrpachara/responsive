dec2002690296c9ab914e6bb9873a85bf11c2465
mrpachara <mrpachara@gmail.com>
2014-12-29 11:30:11 +0700

    add full page content
    add all content page
    fix fade out IE BUG

--- js/autosMart.js
+++ js/autosMart.js
@@ -1,23 +1,97 @@
 var autosMart = angular.module('AutosMart', []);
 
-(function(document, autosMart){
+(function(document, $, angular, autosMart){
 	'use strict';
 
-	autosMart.controller('MenuController', ['$scope', '$http', function($scope, $http){
+	var commandData = {
+		'lang': 'eng'
+	};
+
+	var loadContentFn = function(){
+		var link = location.hash.substring(2);
+		var $a = $('#cp-menu a[href="' + link + '"]');
+		if($a.length === 0) $a = $('#cp-menu a[href]');
+		history.replaceState(null, $a.first().text(), '#!' + $a.first().attr('href'));
+		$a.first().trigger('click');
+	};
+
+	autosMart.controller('CommandController', ['$scope', function($scope){
+		$scope.data = commandData;
+		$scope.toggleLang = function(){
+			if($scope.data.lang === 'eng'){
+				$scope.data.lang = 'th';
+			} else{
+				$scope.data.lang = 'eng';
+			}
+		};
+	}]);
+
+	autosMart.controller('MenuController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout){
 		$http.get('resources/menu.json')
 			.success(function(data, status, headers, config) {
 				$scope.menus = data;
-				$scope.searchTerm = '';
+
+				$timeout(function(){
+					$(window).on('popstate.global', loadContentFn).trigger('popstate');
+				}, 100);
 			})
 		;
 	}]);
 
-	/*
-	var searchData = {
-		'term': '1234'
-	};
-	autosMart.controller('SearchController', ['$scope', function($scope){
-		$scope.data = searchData;
+	autosMart.controller('MyTestController', ['$scope', function($scope){
+		$scope.data = {'message': 'Home'};
+		console.log('zzz', $scope);
 	}]);
-	*/
-})(window.document, autosMart);
+
+	$(function(){
+		var $html = $('html');
+		var $cpMenu = $('#cp-menu');
+		var $lyContent = $('#ly-content');
+
+		$('#cp-menu').on('click.global', 'a', function(ev){
+			ev.preventDefault();
+
+			var $this = $(this);
+
+			$cpMenu.removeClass('st-active');
+			$html.removeClass('st-overlap');
+
+			$cpMenu.children().removeClass('st-active');
+			$this.closest('.cp-menu-item').addClass('st-active');
+
+			if($this.data('showTitle')){
+				$html.addClass('st-show-title');
+			} else{
+				$html.removeClass('st-show-title');
+				$('#ly-title').removeAttr('style');
+				$('#cp-title-content').removeAttr('style');
+			}
+
+			if(typeof ev.originalEvent != 'undefined') history.pushState(null, $this.text(), '#!' + $this.attr('href'));
+
+			$.ajax($this.attr('href'), {
+				'type': 'get',
+				'cache': false
+			})
+				.done(function(data){
+					var doc = (new DOMParser()).parseFromString(data, "text/html");
+					$lyContent.empty();
+					var $script = $('script', doc);
+					$script.each(function(){
+						$lyContent.append(document.importNode(this, true));
+					});
+					$script.remove();
+
+					$('body>*', doc).each(function(){
+						var newElement = document.importNode(this, true);
+						$lyContent.append(newElement);
+					});
+					/*
+					angular.element(document).injector().invoke(function($compile) {
+					});
+					*/
+				})
+			;
+		})
+	});
+})(window.document, window.jQuery, window.angular, autosMart);
--- js/layout.js
+++ js/layout.js
@@ -20,35 +20,38 @@
 			var scrollTop = $this.scrollTop();
 
 			$html[(scrollTop == 0)? 'removeClass' : 'addClass']('st-float');
-			/*
-			$('button.cmd-toggle').each(function(){
-				$('#' + $(this).val()).removeClass('st-active');
-			});
-			*/
 
-			if((scrollTop < $lyTitle.outerHeight()) && (($this.height() + scrollTop) < $(document).height())){
-				var window_height = $this.height();
-				var lyTitle_ratio = (window_height - scrollTop)/window_height;
-				if(lyTitle_ratio < 0) lyTitle_ratio = 0;
+			if($html.hasClass('st-show-title')){
+				if((scrollTop < $lyTitle.outerHeight()) && (($this.height() + scrollTop) < $(document).height())){
+					var window_height = $this.height();
+					var lyTitle_ratio = (window_height - scrollTop)/window_height;
+					if(lyTitle_ratio < 0) lyTitle_ratio = 0;
 
-				$lyTitle.attr('style', 'height: ' + (lyTitle_ratio * 100) + '%');
+					$lyTitle.attr('style', 'height: ' + (lyTitle_ratio * 100).toFixed(2) + '%;');
 
-				setTimeout(function(){
-					var lyTitle_opacityRatio = ($lyTitle.outerHeight() - $this.scrollTop()) / $this.height();
-					if(lyTitle_opacityRatio < 0) lyTitle_opacityRatio = 0;
+					//setTimeout(function(){
+						var lyTitle_opacityRatio = ($lyTitle.outerHeight() - $this.scrollTop()) / $this.height();
+						if(lyTitle_opacityRatio < 0) lyTitle_opacityRatio = 0;
 
-					$('.el-content', $lyTitle).attr('style', 'opacity: ' + lyTitle_opacityRatio + ';');
-				}, 1);
+						var $cpTitleContent = $('#cp-title-content', $lyTitle);
+						$cpTitleContent.attr('style', 'opacity: ' + lyTitle_opacityRatio.toFixed(2) + '; transform: translateY(' + parseInt(scrollTop/2) + 'px);');
+					//}, 1);
+				}
 			}
 		});
 
-		$(document).on('click.global', '.cmd-toggle', function(ev){
+		$(document).on('click.layout', '.cmd-toggle', function(ev){
 			var $this = $(this);
 
 			var $target = $('#' + $this.val());
 			$target.removeAttr('style');
 
 			$target.toggleClass('st-active');
+
+			if($target.attr('id') == 'cp-menu'){
+				$('html')[($target.hasClass('st-active'))? 'addClass' : 'removeClass']('st-overlap');
+			}
+
 			setTimeout(function(){
 				$target.attr('style', 'overflow-y: auto;');
 			}, 500);
